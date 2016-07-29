e57039c20f1b8e2f4c788bfc5484bfa7fb6d91e5
Naohiro Aota <naohiro.aota@hgst.com>
2016-07-29 21:01:06 +0900

    in-container build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+target=$*
+test -z "$target" && exit 1
+
+id=build-${RANDOM}
+machinectl clone base "${id}" || exit 1
+systemd-nspawn -q -M "${id}" \
+	       --bind /usr/portage --bind /var/lib/layman \
+	       --setenv="LANG=C" --setenv="LC_ALL=C" \
+	       emerge -1btkq $target
+result=$?
+machinectl remove "${id}"
+exit $result

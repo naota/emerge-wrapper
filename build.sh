c8dd42a0eab9e65d87c5ad4782aef9adb54cae91
Naohiro Aota <naota@gentoo.org>
2016-08-01 08:33:21 +0900

    mount layamn tree readonly

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ test -z "$target" && exit 1
 id=build-${RANDOM}
 machinectl clone base "${id}" || exit 1
 systemd-nspawn -q -M "${id}" \
-	       --bind /usr/portage --bind /var/lib/layman \
+	       --bind /usr/portage --bind-ro /var/lib/layman \
 	       --setenv="LANG=C" --setenv="LC_ALL=C" \
 	       emerge -1btkq $target
 result=$?

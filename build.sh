519d6782337ed89dcc23c2ac71375bc255e28e9a
Naohiro Aota <naohiro.aota@hgst.com>
2016-08-02 16:38:34 +0900

    use "dmerge-" prefix for the images

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 target=$*
 test -z "$target" && exit 1
 
-id=build-${RANDOM}
-machinectl clone base "${id}" || exit 1
+BASE="dmerge-base"
+id="dmerge-build-${RANDOM}"
+machinectl clone "${BASE}" "${id}" || exit 1
 systemd-nspawn -q -M "${id}" \
 	       --bind /usr/portage --bind-ro /var/lib/layman \
 	       --setenv="LANG=C" --setenv="LC_ALL=C" \

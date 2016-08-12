45025ed0ea47f0a9cc44b5ef243af935f34cc1f4
Naohiro Aota <naota@elisp.net>
2016-08-12 09:41:13 +0900

    build-server: initial

---
+++ build-server/build-server.go
@@ -0,0 +1,80 @@
+//go:generate stringer -type=BuilderError
+
+package main
+
+import (
+	"fmt"
+	"github.com/msgpack-rpc/msgpack-rpc-go/rpc"
+	"github.com/satori/go.uuid"
+	"log"
+	"net"
+	"reflect"
+)
+
+type BuilderID string
+type BuilderInfo struct {
+	id BuilderID
+}
+
+type BuildServer map[string]reflect.Value
+
+type BuilderError int
+
+const (
+	NoSuchBuilder BuilderError = iota
+)
+
+var numProcs uint
+var builders map[BuilderID]BuilderInfo
+
+func (builder BuildServer) Resolve(name string, arguments []reflect.Value) (reflect.Value, error) {
+	return builder[name], nil
+}
+
+func Allocate(cnt uint) ([]BuilderID, fmt.Stringer) {
+	n := cnt
+	if cnt > numProcs {
+		n = numProcs
+	}
+	numProcs -= n
+
+	var i uint
+	var ids []BuilderID
+	for i = 0; i < n; i++ {
+		b := newBuilder()
+		builders[b.id] = b
+		ids = append(ids, b.id)
+	}
+
+	return ids, nil
+}
+
+func newBuilder() BuilderInfo {
+	b := BuilderInfo{}
+	b.id = BuilderID(uuid.NewV4().String())
+	return b
+}
+
+func Free(id BuilderID) (bool, fmt.Stringer) {
+	_, ok := builders[id]
+	if !ok {
+		return false, NoSuchBuilder
+	}
+	delete(builders, id)
+	return true, nil
+}
+
+func main() {
+	numProcs = 4
+
+	builder := BuildServer{}
+
+	serv := rpc.NewServer(builder, true, nil)
+	l, err := net.Listen("tcp", "127.0.0.1:50000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	serv.Listen(l)
+	go serv.Run()
+}
